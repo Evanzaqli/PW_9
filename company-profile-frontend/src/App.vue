34f8cd54005083d1de4a823dc1dcd41a54cb25ef
Evan Zaqli <evanzaqli1@gmail.com>
2024-06-06 17:05:22 +0700

    p

---
+++ company-profile-frontend/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+    <div id="app">
+        <nav>
+            <ul>
+                <li><router-link to="/">Home</router-link></li>
+                <li><router-link to="/About">About</router-link></li>
+                <li><router-link to="/Services">Services</router-link></li>
+            </ul>
+        </nav>
+    <Router/>
+    </div>
+   </template>
+   <script>
+   export default {
+    name: 'App'
+   }
+   </script>
+   <style>
+   nav ul {
+    list-style-type: none;
+    padding: 0;
+   }
+   nav ul li {
+    display: inline;
+    margin-right: 10px;
+   }
+   </style>
